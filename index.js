410f4d0ead72f290211be74aed6cdd4ad1b1b489
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-20 22:16:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+let eventName = "birthday"
+function writeCards(names, eventName) {
+const thankYouCards = [];
+
+for (const name of names) {
+    const message= `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+    thankYouCards.push(message);
+}
+return thankYouCards;
+}
+function countDown(i){
+let countdown = "";
+while (i>= 0){
+    console.log(i);
+    i--;
+}
+
+}
+countdown();
